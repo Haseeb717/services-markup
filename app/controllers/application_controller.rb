c7667a5c1132f2ce6c7a9a18d0ca0843d73c5d2f
haseeb <haseeb@yallamotor.com>
2020-07-24 19:24:32 +0500

    Upload CV setup

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,10 +13,10 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     # For additional fields in app/views/devise/registrations/new.html.erb
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:user_name, :first_name, :last_name, :avatar])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:user_name, :first_name, :last_name, :avatar,:resume])
 
     # For additional in app/views/devise/registrations/edit.html.erb
-    devise_parameter_sanitizer.permit(:account_update, keys: [:user_name, :first_name, :last_name, :avatar])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:user_name, :first_name, :last_name, :avatar,:resume])
   end
 
   private
